eca241df23e804a533576d5cdc181f665b978599
Leshis <94430516+Leshis@users.noreply.github.com>
2026-05-18 22:43:08 +0100

    Refactor ArticleContent.vue to eliminate duplicate code
    Removed duplicate onMounted function and import of DOMPurify.

--- components/ArticleContent.vue
+++ components/ArticleContent.vue
@@ -75,8 +75,6 @@ const sanitizedContent = ref('')
 
 onMounted(async () => {
   const DOMPurify = (await import('dompurify')).default
-onMounted(async () => {
-  const DOMPurify = (await import('dompurify')).default
   const safe = DOMPurify.sanitize(props.article.content ?? '')
   if (!safe) { sanitizedContent.value = ''; return }
   const parser = new DOMParser()
