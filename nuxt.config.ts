ed9a32396adab92d538594e76d94392d5a361eed
Leshis <94430516+Leshis@users.noreply.github.com>
2026-05-18 22:28:41 +0100

    Remove image provider setting from nuxt.config
    Removed the 'provider' setting from the image configuration.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,6 @@ export default defineNuxtConfig({
     alias: {
       supabase: process.env.SUPABASE_URL ? process.env.SUPABASE_URL + '/storage/v1/object/public/articles' : ''
     },
-    provider: 'ipx',
     format: ['avif', 'webp']
   },
   routeRules: {
