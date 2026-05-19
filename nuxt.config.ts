c17a92df53c715d74b4aeb29f518cc5dad75557e
Leshis <94430516+Leshis@users.noreply.github.com>
2026-05-19 16:40:20 +0100

    Update prerender configuration in nuxt.config.ts
    Add ignore pattern for Netlify images in prerender.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -93,7 +93,8 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       routes: ['/'],
-      crawlLinks: true
+      crawlLinks: true,
+      ignore: [/^\/\.netlify\/images/]
     }
   }
 })
