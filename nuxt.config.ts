71e85ba428c29215abb33b72bc954b40e967eae8
Leshis <94430516+Leshis@users.noreply.github.com>
2026-05-19 09:16:02 +0100

    Fix route rules for contact page prerendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,9 +25,7 @@ export default defineNuxtConfig({
     '/': { prerender: true },
     '/about': { prerender: true },
     '/privacy-policy': { prerender: true },
-    '/contact': { prerender: true },
-    '/preview/**': { ssr: false },
-    '/article/**': { ssr: true },
+    '/contact': { prerender: true }
   },
   
   security: {
